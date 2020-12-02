9284ea374e22e045ada8311305d4a906b7209bd8
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-02 13:24:59 +0300

    add 4 and 5

---
+++ homework2/5. rating.py
@@ -0,0 +1,15 @@
+my_list = [7, 5, 3, 3, 2]
+
+print(my_list)
+
+digit = int(input("Введите число: "))
+
+for elem in range(len(my_list)):
+    if my_list[elem] == digit or my_list[elem] > digit and my_list[elem + 1] < digit:
+        my_list.insert(elem + 1, digit)
+        break
+    elif my_list[0] < digit:
+        my_list.insert(0, digit)
+    elif my_list[-1] > digit:
+        my_list.append(digit)
+print(f"Текущий список - {my_list}")
