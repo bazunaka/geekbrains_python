670d6d7ab6752a037746c8976684c80dbfafe8ed
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-02 19:46:06 +0300

    modify 5 (add exit)

--- homework2/5. rating.py
+++ homework2/5. rating.py
@@ -2,14 +2,17 @@ my_list = [7, 5, 3, 3, 2]
 
 print(my_list)
 
-digit = int(input("Введите число: "))
+digit = int(input("Введите число (или 000 для выхода): "))
 
-for elem in range(len(my_list)):
-    if my_list[elem] == digit or my_list[elem] > digit and my_list[elem + 1] < digit:
-        my_list.insert(elem + 1, digit)
-        break
-    elif my_list[0] < digit:
-        my_list.insert(0, digit)
-    elif my_list[-1] > digit:
-        my_list.append(digit)
-print(f"Текущий список - {my_list}")
+while digit != 000:
+    for elem in range(len(my_list)):
+        if my_list[elem] == digit:
+            my_list.insert(elem + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[elem] > digit and my_list[elem + 1] < digit:
+            my_list.insert(elem + 1, digit)
+    print(f"Текущий список - {my_list}")
