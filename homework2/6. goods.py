6d6f64a296dcd8564a70aecf2fea7b15eee1abf6
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-03 13:10:33 +0300

    modify 6

--- homework2/6. goods.py
+++ homework2/6. goods.py
@@ -1,15 +1,17 @@
-goods = int(input("Введите количество товара "))
+goods = []
 n = 1
-my_dict = []
-my_list = []
-my_analys = {}
-while n <= goods:
+
+features = {'наим': '', 'цена': '', 'кол-во': '', 'ед': ''}
+analytics = {'наим': [], 'цена': [], 'кол-во': [], 'ед': []}
+
+
+
+"""while n <= goods:
     my_dict = dict({'название': input("введите название "), 'цена': input("Введите цену "),
                     'количество': input('Введите количество '), 'eд': input("Введите единицу измерения ")})
     my_list.append((n, my_dict))
     n += 1
+    #print(my_list)
     #my_analys = dict(
     #    {'название': my_dict.get('название'), 'цена': my_dict.get('цена'), 'количество': my_dict.get('количество'),
-    #     'ед': my_dict.get('ед')})
-print(my_list)
-print(my_analys)
+    #     'ед': my_dict.get('ед')})"""
