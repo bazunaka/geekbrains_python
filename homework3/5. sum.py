988c90c044ad81152d82b2a9466f15485f7ded7b
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-06 22:38:49 +0300

    finish 5

--- homework3/5. sum.py
+++ homework3/5. sum.py
@@ -8,12 +8,39 @@ while True:
         break
     print(digits)'''
 
-while True:
-    digit = input("")
-    digit = digit.split(" ")
-    a = 0
+stop_index = None
+result = 0
 
-    while a < len(digit):
+#отдельная функция преобразования в int
+def convert (item):
+
+    int_item = int(item.split('.')[0])
+
+    return int_item
+
+#функция для суммирования
+
+def sum(iterable, start = 0):
+    result = start
+
+    for item in iterable:
+        result += item
+
+    return result
+
+while True:
+    digit = input("Введите числа через пробел: ").split()
+
+    stop_index = digit.index('q')
+    digit = digit[:stop_index]
+
+    digit = [convert(i) for i in digit]
+
+    result += sum(digit)
+
+    print(result)
+
+    '''while a < len(digit):
         digit[a] = int(digit[a])
         a = a + 1
     result = 0
@@ -21,4 +48,4 @@ while True:
     for c in digit:
         result = result + c
 
-    print(result)
+    print(result)'''
