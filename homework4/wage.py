e5a0a9546483eab3e9d43fe09eaaf6b72065b117
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-08 20:07:01 +0300

    error!

--- homework4/wage.py
+++ homework4/wage.py
@@ -1,16 +1,27 @@
-from sys import argv
+'''from sys import argv
 
 # pylint: disable=unbalanced-tuple-unpacking
-time, salary, bonus = argv
 
-'''t_time = int(t_time)
+t_time, salary, bonus = argv
+
+t_time = int(t_time)
 salary = int(salary)
 bonus = int(bonus)
 result = t_time * salary + bonus
 print(f'Заработная плата {result}')'''
 
-time = float(input('Выработка в часах: '))
-salary = int(input('Ставка: '))
-bonus = int(input('Премия: '))
-res = time * salary + bonus
-print(f'заработная плата сотрудника  {res}')
+#выдает ошибку в терминале: 
+#MacBook-Pro-Andrej:homework4 andrejsuvorov$ python3 wage.py 1 1 1
+#Traceback (most recent call last):
+#  File "wage.py", line 4, in <module>
+#    time, salary, bonus = argv
+#ValueError: too many values to unpack (expected 3)
+
+def salary():
+    time = float(input('Выработка в часах: '))
+    salary = int(input('Ставка: '))
+    bonus = int(input('Премия: '))
+    res = time * salary + bonus
+    print(f'заработная плата сотрудника  {res}')
+
+salary()
