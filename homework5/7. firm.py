0d1ec724db31c8659faa882fa852128fba7e00d5
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-14 13:50:47 +0300

    average an print profit

--- homework5/7. firm.py
+++ homework5/7. firm.py
@@ -12,11 +12,10 @@ with open('homework5/file_7.txt', 'r') as file:
             profit = profit + profit_dict[name]
             i += 1
 
-            #print(profit)
-            #print(profit_dict)
-
     if i != 0:
         average = profit / i
         print(f'Средняя прибыль = {average}')
     else:
         print(f'Средняя прибыль отсутствует!')
+
+    print(f'Прибыль каждой компании = {profit_dict}')
