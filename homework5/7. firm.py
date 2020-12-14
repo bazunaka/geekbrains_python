a1687e8ffa016cfc1cf3c764d87e17185c9281ee
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-14 13:47:16 +0300

    modify 7

--- homework5/7. firm.py
+++ homework5/7. firm.py
@@ -1,6 +1,22 @@
 import json
 
+profit_dict = {}
+profit = 0
+i = 0 #для подсчета кол-ва итераций -> среднее значение
+
 with open('homework5/file_7.txt', 'r') as file:
     for line in file:
         name, fir, earning, damage = line.split()
-        
+        profit_dict[name] = int(earning) - int(damage)
+        if profit_dict[name] >= 0:
+            profit = profit + profit_dict[name]
+            i += 1
+
+            #print(profit)
+            #print(profit_dict)
+
+    if i != 0:
+        average = profit / i
+        print(f'Средняя прибыль = {average}')
+    else:
+        print(f'Средняя прибыль отсутствует!')
