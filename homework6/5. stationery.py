83978990d33a4166eac77dbbe0c6743e41ec1bfc
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-16 22:09:33 +0300

    create 5 (not finish)

---
+++ homework6/5. stationery.py
@@ -0,0 +1,21 @@
+class Stationary:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+class Pen(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки ручкой'
+
+
+class Pencil(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки карандашом'
