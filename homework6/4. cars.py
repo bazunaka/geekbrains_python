fad4cfa532cc478f58d8b381f0c6f222483f8e2e
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-17 13:51:17 +0300

    4 finish

--- homework6/4. cars.py
+++ homework6/4. cars.py
@@ -20,3 +20,47 @@ class Car:
 
     def show_speed(self):
         return f'Текущая скорость {self.name} = {self.speed}'
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость рабочего автомобиля {self.name} равна {self.speed}')
+
+        if self.speed > 40:
+            return f'Скорость {self.name} выше разрешенной!'
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость городского автомобиля {self.name} равна {self.speed}')
+
+        if self.speed > 60:
+            return f'Скорость {self.name} выше разрешенной!'
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+audi = SportCar(100, 'Red', 'Audi', False)
+kia = TownCar(70, 'White', 'Kia', False)
+lada = WorkCar(50, 'Rose', 'Lada', False)
+ford = PoliceCar(90, 'Blue',  'Ford', True)
+
+print(audi.show_speed())
+print(kia.show_speed())
+print(ford.show_speed())
+
+print(audi.turn_left())
+print(ford.turn_right())
+
+print(lada.stop())
+print(kia.go())
