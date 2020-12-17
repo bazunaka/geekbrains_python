aa255d603f2689c438d7a0a61a16d254564a444f
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-17 13:59:35 +0300

    5 finish

--- homework6/5. stationery.py
+++ homework6/5. stationery.py
@@ -19,3 +19,17 @@ class Pencil(Stationary):
 
     def draw(self):
         return f'Вы взяли {self.title}. Запуск отрисовки карандашом'
+
+class Handle(Stationary):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def draw(self):
+        return f'Вы взяли {self.title}. Запуск отрисовки маркером'
+
+pen = Pen('Ручка')
+pencil = Pencil('Карандаш')
+handle = Handle('Маркер')
+print(pen.draw())
+print(pencil.draw())
+print(handle.draw())
