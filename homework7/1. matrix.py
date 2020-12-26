4cf12eb3979b672f68a95ef56a97fc34ebdf3922
bazunaka_macbook <bazunaka32@gmail.com>
2020-12-26 17:19:52 +0300

    create 1 and 2

---
+++ homework7/1. matrix.py
@@ -0,0 +1,11 @@
+class Matrix:
+    def __init__(self, list1, list2):
+        self.list1 = list1
+        self.list2 = list2
+
+    def __add__(self):
+        matr = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+        
+
+    def __str__(self):
---
+++ homework7/2. textil.py
@@ -0,0 +1,29 @@
+class Textil:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_coat(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_jacket(self):
+        return self.height * 2 + 0.3
+
+    @property
+    def get_square_full(self):
+        return str(f'Площадь общая ткани \n'
+                   f' {(self.width / 6.5 + 0.5) + (self.height * 2 + 0.3)}')
+
+class Coat(Textil):
+    def __init__(self, width, height):
+        self.square_coat = self.width / 6.5 + 0.5
+
+    def __str__(self):
+        return f'Площадь на пальто {self.square_coat}'
+
+class Jacket(Textil):
+    def __init__(self, width, height):
+        self.square_jacket = self.height / 2 + 0.3
+
+    def __str__(self):
+        return f'Площадь на костюм {self.square_jacket}'
