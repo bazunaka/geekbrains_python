e0ef91ebe3aa0b3472e1806749a6550af39f54e6
bazunaka <bazunaka32@gmail.com>
2021-02-10 19:35:21 +0300

    dump_help_keyword

--- mysql/homework2/command.sh
+++ mysql/homework2/command.sh
@@ -1,31 +1,41 @@
-//Создаем файл
+#Создаем файл
 touch .my.cnf
 sudo nano .my.cnf
 
-//Добавляем 
+#Добавляем 
 [client]
 user=root
 password=
 
-//Доступ к файлу
+#Доступ к файлу
 chmod 0600 .my.cnf
 
-//Создаем БД
+#Создаем БД
 mysql
 create database example;
 use example;
 
 create table users (id SERIAL, name VARCHAR(200) NOT NULL UNIQUE);
 
-//Добавляем записи
+#Добавляем записи
 insert into users VALUES (1, 'Ivan');
 insert into users VALUES (2, 'Petr');
 
-//Создаем dump БД example
+#Создаем dump БД example
 mysqldump example >> example.sql
 
-//Создаем БД sample
+#Создаем БД sample
 mysql -e 'create database sample'
 
-//Разворачиваем dump
+#Разворачиваем dump
 mysql sample < example.sql
+
+#Создаем dump из таблицы help_keywords первых 100 строк
+mysqldump mysql help_keyword --where='TRUE ORDER BY help_keyword_id LIMIT 100'  > dump.sql
+
+#Тут возник вопрос
+#При использовании ключа -w, а не --where выдавалась синтаксическая ошибка.
+#You have an error in your SQL syntax; 
+#check the manual that corresponds to your MySQL server version
+#for the right syntax to use near '=TRUE LIMIT 100' at line 1 (1064)
+#Потом разобрался: в документации для версии 8 только --where.
