531c62ba9bf2c05162a069dedf3273f3bed78c86
bazunaka <bazunaka32@gmail.com>
2021-02-24 20:47:22 +0300

    create fk for posts

--- mysql/homework6/homework6.sql
+++ mysql/homework6/homework6.sql
@@ -281,3 +281,23 @@ alter table friendship
 	add constraint friendship_user_id_fk
 		foreign key (user_id) references users (id)
 			on delete cascade;
+
+alter table media
+	add constraint media_types_id_fk
+		foreign key (media_type_id) references media_types (id)
+			on delete cascade;
+
+alter table posts
+	add constraint posts_comm_id_fk
+		foreign key (community_id) references communities (id)
+			on delete cascade;
+
+alter table posts
+	add constraint posts_media_id_fk
+		foreign key (media_id) references media (id)
+			on delete cascade;
+
+alter table posts
+	add constraint posts_user_id_fk
+		foreign key (user_id) references users (id)
+			on delete cascade;
