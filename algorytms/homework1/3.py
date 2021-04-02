ef7a128a1d8debdf6658a0ccbe9201db687a536a
bazunaka-macbook <bazunaka32@gmail.com>
2021-04-02 21:18:10 +0300

    finish

--- algorytms/homework1/3.py
+++ algorytms/homework1/3.py
@@ -17,10 +17,6 @@ def f1(dict_obj):
     result = collections.Counter(dict_obj).most_common(3) #O(n)
     return result
 
-def f3(dict_obj):
-    m = max(dict_obj.values()) #O(n)
-    return list(filter(lambda t: t[1]==m, dict_obj.items()))[0][1] 
-
 #вывод
 print(f1(company_profit))
 
@@ -28,6 +24,3 @@ result = list(company_profit.items())
 result.sort(key = lambda i: i[1], reverse = True) #O(n log n)
 for i in range(3):
     print(result[i][0], '=', result[i][1])
-
-#print(f3(company_profit))  
-
