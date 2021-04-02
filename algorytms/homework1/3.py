5f2d6a9866cf0e568c385b339c40285a7b65c5cc
bazunaka-macbook <bazunaka32@gmail.com>
2021-04-02 13:59:00 +0300

    modify3

--- algorytms/homework1/3.py
+++ algorytms/homework1/3.py
@@ -14,17 +14,20 @@ company_profit = {  "comp_1": 10000,
 
 def f1(dict_obj):
     result = []
-    result = collections.Counter(dict_obj).most_common(3)
+    result = collections.Counter(dict_obj).most_common(3) #O(n)
     return result
-
-def f2(dict_obj):
-    result = list(dict_obj.items())
-    result.sort(key = lambda i: i[1], reverse = True)
 
 def f3(dict_obj):
     m = max(dict_obj.values()) #O(n)
     return list(filter(lambda t: t[1]==m, dict_obj.items()))[0][1] 
 
+#вывод
 print(f1(company_profit))
-print(f3(company_profit))  
+
+result = list(company_profit.items())
+result.sort(key = lambda i: i[1], reverse = True) #O(n log n)
+for i in range(3):
+    print(result[i][0], '=', result[i][1])
+
+#print(f3(company_profit))  
 
