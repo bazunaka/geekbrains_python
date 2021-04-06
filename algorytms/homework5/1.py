66ff7f10b9df7c6f058e3df4cff04e46ff11ff9c
bazunaka-macbook <bazunaka32@gmail.com>
2021-04-06 13:28:56 +0300

    modify1

--- algorytms/homework5/1.py
+++ algorytms/homework5/1.py
@@ -6,8 +6,8 @@ while True:
 
 #вызывает функцию для предоставления отсутствующих значений
 companies = collections.defaultdict() #Возвращает новый объект, подобный словарю
-profit = collections.deque() #контейнер в виде списка, обобщение стеков и очередей
-unprofit = collections.deque()
+profit_col = collections.deque() #контейнер в виде списка, обобщение стеков и очередей
+unprofit_col = collections.deque()
 all_profit = 0
 quarter = 4
 
@@ -24,14 +24,14 @@ for i in range(n):
 midlle_profit = all_profit / n
 for i, item in companies.items():
     if item >= midlle_profit:
-        profit.append(i)
+        profit_col.append(i)
     else:
-        unprofit.append(i)
+        unprofit_col.append(i)
 
 print(f'Общая средняя прибыль: {midlle_profit}')
-print(f'Прибыль выше среднего у {len()} компаний: ')
-for name in profit:
+print(f'Прибыль выше среднего у {len(profit_col)} компаний: ')
+for name in profit_col:
     print(name)
-print(f'Прибыль ниже среднего у {len()} компаний: ')
-for name in unprofit:
+print(f'Прибыль ниже среднего у {len(unprofit_col)} компаний: ')
+for name in unprofit_col:
     print(name)
