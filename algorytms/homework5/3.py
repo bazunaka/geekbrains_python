22be2d9422daefc600cc078d35378bc7ecf641ed
bazunaka-macbook <bazunaka32@gmail.com>
2021-04-07 19:10:57 +0300

    modify3

--- algorytms/homework5/3.py
+++ algorytms/homework5/3.py
@@ -13,3 +13,18 @@ numbers = range(1000)
 
 deq = deque(numbers)
 lst = list(numbers)
+
+@time_decorator
+def add_in_list(list_obj):
+    list_obj.append('1001')
+    return list_obj
+
+@time_decorator
+def add_in_deq(deq_obj):
+    deq_obj.append('1001')
+    return deq_obj
+
+new_list, list_time = add_in_list(lst)
+new_deq, deq_time = add_in_deq(deq)
+print(list_time)
+print(deq_time)
